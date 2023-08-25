a3bfe1c73a29a6e10def8836572c30d8283151ad
Guilhermee19 <guilherme@noclaftech.com>
2023-08-25 13:23:06 -0300

    feat: router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'setup', loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule) }];
+  { path: '', loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
