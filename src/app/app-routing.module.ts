3868eec5d3aab6c4fcb4e54f5f0fd63f2b7860a7
Guilhermee19 <guilherme@noclaftech.com>
2024-05-25 02:19:45 -0300

    feat: page deck

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule) }];
+  { path: '', loadChildren: () => import('./pages/setup/setup.module').then(m => m.SetupModule) },
+  { path: 'deck', loadChildren: () => import('./pages/deck/deck.module').then(m => m.DeckModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
